3ac67b0f150189a4e989573c2448dbb3245d0f09
MaykonOliveira <moliveira.anschau@gmail.com>
2020-02-04 20:56:49 -0300

    Add File model

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,10 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 import Recipient from '../app/models/Recipient';
 
-const models = [User, Recipient];
+const models = [User, File, Recipient];
 
 class Database {
   constructor() {
