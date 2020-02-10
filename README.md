01d31ea4b910fe155812163c7de87a7233862fd8
Maykon Oliveira <maykon.oliveira@neoway.com.br>
2020-02-10 09:03:42 -0300

    Added logic too orders end controller

--- README.md
+++ README.md
@@ -47,7 +47,6 @@ To run this project you need execute the following steps:
 
 - [ ] Send an email to the delivery man when have a new order in your name.
 - [ ] Implement limit of start order per day.
-- [ ] Implement the OrdersEndController logic.
 
 ## License
 
--- src/app/controllers/OrdersEndController.js
+++ src/app/controllers/OrdersEndController.js
@@ -1,10 +1,51 @@
-class OrdersStartController {
+import { Op } from 'sequelize';
+import * as Yup from 'yup';
+
+import Order from '../models/Order';
+
+class OrdersEndController {
   async update(req, res) {
-    return res.json({
-      deliveryman: req.params.deliverymanId,
-      order: req.params.orderId,
+    const schema = Yup.object().shape({
+      signature_id: Yup.number()
+        .integer()
+        .required(),
     });
+
+    if (!(await schema.isValid(req.params))) {
+      return res
+        .status(400)
+        .json({ error: 'Schema of the params is invalid.' });
+    }
+
+    const now = new Date();
+
+    const { orderId, deliverymanId, signature_id } = req.params;
+
+    const order = await Order.findOne({
+      where: {
+        id: orderId,
+        deliveryman_id: deliverymanId,
+        canceled_at: null,
+        start_date: {
+          [Op.not]: null,
+        },
+        end_date: null,
+      },
+    });
+
+    if (!order) {
+      return res.status(404).json({
+        error: `Not found started order with id ${orderId} for the deliveryman ${deliverymanId}`,
+      });
+    }
+
+    const updatedOrder = await order.update({
+      end_date: now,
+      signature_id,
+    });
+
+    return res.json(updatedOrder);
   }
 }
 
-export default new OrdersStartController();
+export default new OrdersEndController();
